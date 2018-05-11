1619e69e62d17d1e1a89f1a2d38c7d79acba2458
andresum97 <andresum1309@gmail.com>
2018-05-11 11:29:32 -0600

    Comprobación de funcionamiento en base de datos, y algunos ajustes

--- HDT10.py
+++ HDT10.py
@@ -1,14 +1,14 @@
 from neo4jrestclient.client import GraphDatabase
 from neo4jrestclient import client
  
-db = GraphDatabase("http://localhost:7687", username="neo4j", password="12345")
+db = GraphDatabase("http://localhost:7474", username="neo4j", password="12345")
 #Crear los primero nodos#
 Mario= db.nodes.create(name="Mario Juarez", contact="12653248", speciality="Pediatra")
-Mario.label.add("Doctor")
+Mario.labels.add("Doctor")
 Aspirina= db.nodes.create(name="Aspirina")
-Aspirina.label.add("Medicine")
+Aspirina.labels.add("Medicine")
 Roberto= db.nodes.create(name="Roberto Garcia")
-Roberto.label.add("Patient")
+Roberto.labels.add("Patient")
 Roberto.relationships.create("Visits", Mario, date="11/05/18")
 Mario.relationships.create("Prescribes", Aspirina,amount="500g")
 Roberto.relationships.create("Takes", Aspirina)
