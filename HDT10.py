c221a61e200d9ef30ea031d081df689af3d985da
DESKTOP-Q5M49A3\paulb <paulbelches@gmail.com>
2018-05-11 11:23:31 -0600

    Nodos creados
    Creacion de los primeros 3 nodos

--- HDT10.py
+++ HDT10.py
@@ -1,15 +1,16 @@
 from neo4jrestclient.client import GraphDatabase
 from neo4jrestclient import client
  
-db = GraphDatabase("http://localhost:7474", username="neo4j", password="mypassword")
-
-
+db = GraphDatabase("http://localhost:7687", username="neo4j", password="12345")
+#Crear los primero nodos#
 Mario= db.nodes.create(name="Mario Juarez", contact="12653248", speciality="Pediatra")
-mario.label.add("Doctor")
+Mario.label.add("Doctor")
+Aspirina= db.nodes.create(name="Aspirina")
+Aspirina.label.add("Medicine")
+Roberto= db.nodes.create(name="Roberto Garcia")
+Roberto.label.add("Patient")
+Roberto.relationships.create("Visits", Mario, date="11/05/18")
+Mario.relationships.create("Prescribes", Aspirina,amount="500g")
+Roberto.relationships.create("Takes", Aspirina)
 
-Mario= db.nodes.create(name="Aspirina",amount="500g")
-mario.label.add("Medicine")
-
-Mario= db.nodes.create(name="Roberto Garcia")
-mario.label.add("Patient")
-
+##
